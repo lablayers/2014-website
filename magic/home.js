1eaa076dd1edc3675d45f9ad548240bd6d2ea659
Victor Lourng <hello@victorlourng.com>
2014-01-18 12:06:37 -0500

    Move tracking codes for transparency

--- magic/home.js
+++ magic/home.js
@@ -373,14 +373,8 @@ $(document).on('click', '.skyward', function () {
 // Google Analytics
 // -> If you hate this, I understand. I'm not using your data for evil, I promise!
 // -@ http://analytics.google.com/?referrer=justinbieber
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-33381009-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
-    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+
+//    (Moved to layout.jade)
 
 // SNOW!
 // -> Winter wrap-up planned for March.
