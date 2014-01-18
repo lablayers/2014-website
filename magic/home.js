936e6cdd3803a98f7c24a5b9bbe2df1b39788b99
Victor Lourng <hello@victorlourng.com>
2014-01-18 12:03:53 -0500

    "Enhanced" Analytics and remove header file.

--- magic/home.js
+++ magic/home.js
@@ -371,18 +371,16 @@ $(document).on('click', '.skyward', function () {
 // -> Now that you've made it this far... you've been warned. ;P
 
 // Google Analytics
-// -> If you hate this, I understand.
+// -> If you hate this, I understand. I'm not using your data for evil, I promise!
 // -@ http://analytics.google.com/?referrer=justinbieber
-var _gaq = [
-    ['_setAccount', 'UA-33381009-1'],
-    ['_trackPageview']
-];
-(function(d, t) {
-    var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-    g.src = '//www.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g, s);
-}(document, 'script'));
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33381009-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
 // SNOW!
 // -> Winter wrap-up planned for March.
