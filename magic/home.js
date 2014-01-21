596e31ef9782a5b74bc295bb227f271baeff6086
Victor Lourng <hello@victorlourng.com>
2014-01-20 19:19:58 -0500

    Fix Smooth Scrolling!

--- magic/home.js
+++ magic/home.js
@@ -337,16 +337,14 @@ $(document).on('click', '#unicorn .cart .span4 a', function () {
 
 // Smooth Scrolling
 // -> It's pretty awesome.
-$(document).ready(function(){
-    $('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
-        var target = this.hash,
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 800, 'swing', function () {
-            window.location.hash = target;
-        });
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    var target = this.hash,
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+        window.location.hash = target;
     });
 });
 
