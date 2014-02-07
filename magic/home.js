11da2a9931e65d334b07cc93dab7d7bbdbb55fba
Victor Lourng <hello@victorlourng.com>
2014-02-06 20:21:22 -0500

    Nevermind, that just made my site slower.

--- magic/home.js
+++ magic/home.js
@@ -31,19 +31,19 @@ document.createElement("article");
 
 // Disable Pointer Events on Scroll
 // -@ http://www.thecssninja.com/javascript/pointer-events-60fps
-var body = document.body,
-    timer;
-
-window.addEventListener('scroll', function() {
-  clearTimeout(timer);
-  if(!body.classList.contains('disable-hover')) {
-    body.classList.add('disable-hover')
-  }
-  
-  timer = setTimeout(function(){
-    body.classList.remove('disable-hover')
-  },100);
-}, false);
+// var body = document.body,
+//     timer;
+// 
+// window.addEventListener('scroll', function() {
+//   clearTimeout(timer);
+//   if(!body.classList.contains('disable-hover')) {
+//     body.classList.add('disable-hover')
+//   }
+//   
+//   timer = setTimeout(function(){
+//     body.classList.remove('disable-hover')
+//   },100);
+// }, false);
 
 // jQuery User Settings
 // -> Toggles Menus
