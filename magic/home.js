01505db4a8708f78d2fb193c304c2ee3ea42fd80
Victor Lourng <hello@victorlourng.com>
2014-02-06 20:20:30 -0500

    Disable Pointer Events on Scroll

--- magic/home.js
+++ magic/home.js
@@ -29,6 +29,22 @@ document.createElement("menu");
 document.createElement("tooltip");
 document.createElement("article");
 
+// Disable Pointer Events on Scroll
+// -@ http://www.thecssninja.com/javascript/pointer-events-60fps
+var body = document.body,
+    timer;
+
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+  
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },100);
+}, false);
+
 // jQuery User Settings
 // -> Toggles Menus
 // -@ http://stackoverflow.com/questions/14106651/jquery-toggle-element-based-on-click-of-another-div
